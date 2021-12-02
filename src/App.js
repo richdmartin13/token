86799c4715f7ffda7d298b9cc0489045a21b52eb
Richard Martin <accounts@richdmart.in>
2021-12-01 23:57:45 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { name, tagline, bio, badges, buttons } from "./config";
 import './App.css';
+import { configure } from "@testing-library/dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App vstack">
+      <div className="hstack">
+        <div className="card hstack">
+          
+        </div>
+      </div>
+      <div className="buttons hstack">
+      {buttons.map((data, key) =>
+          <div className="butt">
+
+          </div>
+        )}
+      </div>
+      <div className="bio vstack">
+        <div className="hstack" style={{"width":"100%", "justify-content": "center"}}>
+          <h2>{name}</h2>
+        </div>
+        <h3>{tagline}</h3>
+        <hr></hr>
+        <br/>
+        <h3>About Me</h3>
+        <p>{bio}</p>
+      </div>
+      <br></br>
+      <div className="badges hstack">
+        {badges.map((data, key) =>
+          <div className="badge">
+
+          </div>
+        )}
+      </div>
     </div>
   );
 }
