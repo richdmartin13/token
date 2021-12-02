e0d50292b631b85d815c7957e6ffec11d166888f
Richard Martin <accounts@richdmart.in>
2021-12-01 23:57:55 -0500

    initial commit

---
+++ src/config.js
@@ -0,0 +1,40 @@
+export const name = "Richard Martin"
+export const tagline = "Digital Nomad"
+export const bio = "I'm an International Business graduate from Maryville College, and I spent the past 3 months traveling Europe."
+export const badges = [
+    {
+        name: "richdmartin",
+        url: "https://richdmart.in",
+        icon: "icon"
+    },
+    {
+        name: "swiftech",
+        url: "https://swifte.ch",
+        icon: "icon"
+    },
+    {
+        name: "OneResume",
+        url: "https://oneresume.us",
+        icon: "icon"
+    },
+    {
+        name: "GitHub",
+        url: "https://github.com/richdmartin13",
+        icon: "icon"
+    },
+    {
+        name: "LinkedIn",
+        url: "https://linkedin.com/in/richdmartin",
+        icon: "icon"
+    },
+];
+export const buttons = [
+    {
+        name: "Grab Time",
+        url: "#",
+    },
+    {
+        name: "Message Me",
+        url: "mailto:richdmart.in"
+    }
+]
