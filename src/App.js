5736764ae837b889ca46c16aad8e31e280f1e575
Richard Martin <richdmartin13@users.noreply.github.com>
2021-12-03 02:43:57 -0500

    MIGHTA FUCKED IT UP!!

--- src/App.js
+++ src/App.js
@@ -5,27 +5,24 @@ function App() {
   return (
   <section className="waves">
     <div className="App vstack">
-    <div className="hstack">
       <div className="hstack">
-        <div className="card hstack">
-          
+        <div className="hstack">
+          <div className="card hstack">
+            <h2 className="intro">{name}</h2>
+            <h3 className="intro">{tagline}</h3>
+          </div>
         </div>
-      </div>
-      <div className="buttons hstack">
-      {buttons.map((data, key) =>
+        <div className="buttons hstack">
+          {buttons.map((data, key) =>
           <div className="button">
 
           </div>
         )}
-      </div>
-      <div className="bio vstack">
-        <div className="hstack" style={{"width":"100%", "justify-content": "center"}}>
-          <h2>{name}</h2>
         </div>
-        <h3>{tagline}</h3>
-        <br/>
-        <p>{bio}</p>
-      </div>
+        <div className="bio vstack">
+          <h3>About Me</h3>
+          <p>{bio}</p>
+        </div>
       </div>
       <br></br>
       <div className="badges hstack">
